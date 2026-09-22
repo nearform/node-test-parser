f3f8a74701edcf6d33606654c1ebb981e151f9fa
Simone Pellizzari <simone.pellizzari@nearform.com>
2026-09-22 09:23:05 +0200

    chore(lint): use eslint.config.js and drop the 2021 parser cap
    Closes #255.

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
 
-// Flat-config port of the previous .eslintrc. The .mjs extension is required:
-// this package is "type": "module".
 export default [
   js.configs.recommended,
   prettierRecommended,
   {
     languageOptions: {
-      ecmaVersion: 2021,
       sourceType: 'module',
       globals: {
         ...globals.node,
